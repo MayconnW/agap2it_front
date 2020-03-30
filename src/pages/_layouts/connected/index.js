e0896d144e2f89f10af2aa9fd2f22c9bbd17bbb7
Mayconn Oliveira <mayconnwgo@gmail.com>
2020-03-30 13:50:47 -0300

    As Copy

--- src/pages/_layouts/connected/index.js
+++ src/pages/_layouts/connected/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { Wrapper, Container, Content } from "./style";
+import { Wrapper, Container, Content, Demo } from "./style";
 
 export default function Connected({ children }) {
   return (
     <Wrapper>
       <Container>
         <Content>{children}</Content>
+        <Demo>
+          <span>Cópia Cópia Cópia</span>
+        </Demo>
       </Container>
     </Wrapper>
   );
--- src/pages/_layouts/connected/style.js
+++ src/pages/_layouts/connected/style.js
@@ -4,6 +4,24 @@ export const Wrapper = styled.div`
   min-height: 100vh;
 `;
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  position: relative;
+`;
 
 export const Content = styled.div``;
+
+export const Demo = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.3);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  > span {
+    font-size: 125px;
+    transform: rotateZ(-45deg);
+  }
+`;
